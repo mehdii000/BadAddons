8e30e7d96f9b4a521afaf87658b6f28095d8e716
mehdii000 <mehidonlin00@gmail.com>
2024-10-13 16:28:14 +0100

    Added: PreSpotTracker, Fixed Gui Sizes to fit perfectly, Chat waypoints fetcher

--- src/main/java/mehdi/bad/addons/BadAddons.java
+++ src/main/java/mehdi/bad/addons/BadAddons.java
@@ -34,7 +34,7 @@ import java.util.Iterator;
 @Mod(modid = BadAddons.MODID, version = BadAddons.VERSION, acceptedMinecraftVersions = "[1.8.9]")
 public class BadAddons {
 
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
     public static final String MODID = "badaddons";
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static ArrayList settings = Config.collect(Configs.class);
