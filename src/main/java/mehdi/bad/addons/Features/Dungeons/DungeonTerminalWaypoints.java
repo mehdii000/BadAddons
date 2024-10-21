537faeaffbaadd6a44af7a787d0954741389a54d
mehdii000 <mehidonlin00@gmail.com>
2024-10-22 00:57:21 +0100

    Fixed a game-crashing bug

--- src/main/java/mehdi/bad/addons/Features/Dungeons/DungeonTerminalWaypoints.java
+++ src/main/java/mehdi/bad/addons/Features/Dungeons/DungeonTerminalWaypoints.java
@@ -74,7 +74,7 @@ public class DungeonTerminalWaypoints {
         for (DungeonTerminal term : ALL_TERMINALS) {
             if (!terminalsDone.contains(term) && term.phase == currentPhase) { // Render only terminals not yet done
                 V2RenderUtils.renderBeacon(term.getPos().getX(), term.getPos().getY(), term.getPos().getZ(), Color.BLUE.getRGB(), 0.8f, event.partialTicks);
-                V2RenderUtils.renderBlockModel(term.getPos(), Blocks.redstone_block, event.partialTicks);
+                V2RenderUtils.renderBlockModel(term.getPos(), Blocks.diamond_block, event.partialTicks);
             }
         }
     }
