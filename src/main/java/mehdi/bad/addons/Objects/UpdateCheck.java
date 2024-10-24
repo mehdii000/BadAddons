c28e8c57a7ee3c13bdf028f37fc631e7947823b4
mehdii000 <mehidonlin00@gmail.com>
2024-10-24 14:44:55 +0100

    fixed old mod not deleting when update

--- src/main/java/mehdi/bad/addons/Objects/UpdateCheck.java
+++ src/main/java/mehdi/bad/addons/Objects/UpdateCheck.java
@@ -121,6 +121,17 @@ public class UpdateCheck {
                         File modsFolder = new File(Minecraft.getMinecraft().mcDataDir, "mods");
                         File oldModFile = new File(modsFolder, "BadAddons-" + BadAddons.VERSION + ".jar");
 
+                        // Check and delete the old mod file if it exists
+                        if (oldModFile.exists()) {
+                            try {
+                                oldModFile.deleteOnExit();
+                                ChatLib.chat("§eOld BadAddons version " + BadAddons.VERSION + " has been deleted.");
+                            } catch (Exception e) {
+                                ChatLib.chat("§eCould'nt delete the old mod automatically, please do it manullay");
+
+                            }
+                        }
+
                         // Download the new JAR file
                         try {
                             inputStream = new URL(jarUrl).openStream();
@@ -146,12 +157,6 @@ public class UpdateCheck {
                         } catch (IOException e) {
                             e.printStackTrace();
                             ChatLib.chat("§cFailed to download the new version of BadAddons.");
-                        }
-
-                        // Check and delete the old mod file if it exists
-                        if (oldModFile.exists()) {
-                            oldModFile.deleteOnExit(); // Mark the file for deletion when JVM exits
-                            ChatLib.chat("§[BA] §bOld BadAddons version §c" + BadAddons.VERSION + " §bwill be deleted when the game exits.");
                         }
 
                     } else {
