84090d4dadb988aa26ab71ea9b5868f8ec98bca5
mehdii000 <mehidonlin00@gmail.com>
2025-01-06 00:55:32 +0100

    Fixed Version to 1.5.6

--- src/main/java/mehdi/bad/addons/Features/Kuudra/KuudraHandler.java
+++ src/main/java/mehdi/bad/addons/Features/Kuudra/KuudraHandler.java
@@ -223,10 +223,11 @@ public class KuudraHandler extends MovableModule {
                         .filter(entity -> entity instanceof EntityMagmaCube)
                         .map(entity -> (EntityMagmaCube) entity)
                         .filter(gz -> gz.getSlimeSize() >= 25)
+                        .filter(gz -> gz.posY < 30 || gz.posY > 71)
                         .collect(Collectors.toList());
 
                 for (EntityMagmaCube magmaCube : cubs) {
-                    V2RenderUtils.drawPixelBox(magmaCube.getPositionVector().addVector(-15, 0, -15), Color.GREEN, 15, e.partialTicks);
+                    V2RenderUtils.drawPixelBox(magmaCube.getPositionVector().addVector(-7, 0, -7), Color.GREEN, 14, e.partialTicks);
                 }
 
             }
